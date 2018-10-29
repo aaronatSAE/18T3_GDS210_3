c17b7e295b01a6e99b2ce5cdbd9f851e365d19f4
WilliamRobertKidd <40745570+WilliamRobertKidd@users.noreply.github.com>
2018-10-29 16:09:41 +1100

    Refined Test Movement Script || Still Requires Work
    This lesson I was able to figure out the solution to the problem of "Why isn't AddForce working?". Currently walking and jumping uses AddForce, but not running or rolling. There are now three jumping styles, although they still require major tinkering.
    
    Player direction has now been established, the placeholder player has a "nose" to distinguish where it is looking. With the addition of player direction, walking and running need a slight rework to reflect the player moving where it is looking.
    
    With how jumping currently works, if the player is in mid air long enough they are able to double jump. I will fix this ASAP.

--- BackFlash 2150/Assets/Scripts/MovementScript.cs
+++ BackFlash 2150/Assets/Scripts/MovementScript.cs
@@ -4,70 +4,213 @@ using UnityEngine;
 
 public class MovementScript : MonoBehaviour {
 
-    public bool runningOn;
-    public bool crouchOn;
+        /* State Bools*/
+    bool runningOn;
+    bool crouchOn;
+    bool crouchToggle;
     public bool inJump;
-    public int runningSpeed;
-    public int rollDistance;
+
+    public float runningSpeed;
+    public float rollingSpeed;
+    public float rollDistance;
     public float timeRolling;
     public float rollDelay;
+    public float rollDuration;
     public float airTime;
+    public float jumpDelay;
+    public float jumpDuration;
+
+        /* Movement Speed */
+    public float walkSpeed;
+    public float runSpeed;
+    public float runTime;
+    public float rJumpRunUp;
+
+        /* Jump Heights */
+    public float sJumpHeight;
+    public float rJumpHeight;
+    public float lJumpHeight;
+
+        /* Jump Propulsion */
+    public float rJumpPropulsion;
+    public float rJumpDirection;
+    public float rJumpSpeed = 10;
+
+    public float lJumpPropulsion;
+    public float lJumpDirection;
+    public float lJumpSpeed;
+
+    public float sJumpSpeed;
+
+    public Transform target;
+    public float speed;
+
+    public Rigidbody rb;
+
+    public bool rollingRight;
+    public bool rollingLeft;
+
+    /* Player Look */
+    public bool lookLeft;
+    public Animator lookingAnimation;
 
     // Use this for initialization
     void Start ()
     {
         runningOn = false;
         crouchOn = false;
-	}
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
+            /* Look Direction */
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            lookLeft = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            lookLeft = false;
+        }
+
+        if (lookLeft == true)
+        {
+            lookingAnimation.SetBool("left?", true);
+        } 
+        else
+        {
+            lookingAnimation.SetBool("left?", false);
+        }
+
+            /* Run Button */
         if (Input.GetKey(KeyCode.LeftShift))
         {
             runningOn = true;
         }
-
         else
         {
             runningOn = false;
         }
 
+            /* Crouch Button*/
         if (Input.GetKeyDown(KeyCode.C))
         {
             crouchOn =! crouchOn;
         }
 
+            /* Crouch/Standing Scales */
+        if (crouchOn == true)
+        {
+            transform.localScale = new Vector3(1.1f, 1f, 1.1f);
+
+            if (crouchToggle == false)
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z);
+                crouchToggle = true;
+            }
+            
+        }
+        else
+        {
+            transform.localScale = new Vector3(0.75f, 2, 0.75f);
+            if (crouchToggle == true)
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
+                crouchToggle = false;
+            }
+        }
+
+            /* Jump button */
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.Space))
         {
             inJump = true;
         }
 
-        else
+        //else
+        //{
+        //    inJump = false;
+        //}
+
+            /* Running Jump Direction */
+            if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D) || (Input.GetKeyUp(KeyCode.A) && Input.GetKeyUp(KeyCode.D)))
+            {
+                rJumpDirection = 0;
+            }
+
+            else
+            {
+                if (Input.GetKeyDown(KeyCode.A))
+                {
+                    rJumpDirection = -rJumpPropulsion;
+                }
+
+                if (Input.GetKeyDown(KeyCode.D))
+                {
+                    rJumpDirection = rJumpPropulsion;
+                }
+            }
+
+            /* Leap Jump Direction */
+            if (lookLeft == true)
         {
-            inJump = false;
+            lJumpDirection = -lJumpPropulsion;
+        }
+            else
+        {
+            lJumpDirection = lJumpPropulsion;
         }
 
-        timeRolling += Time.deltaTime;
-        
+            /* Roll direction */
         if (timeRolling > rollDelay)
         {
             if (crouchOn == true)
             {
-                if (Input.GetKey(KeyCode.D))
+                if (Input.GetKeyDown(KeyCode.D))
                 {
-                    transform.Translate(rollDistance,0,0);
                     timeRolling = 0;
+                    rollingRight = true;
                 }
 
-                if (Input.GetKey(KeyCode.A))
+                if (Input.GetKeyDown(KeyCode.A))
                 {
-                    transform.Translate(-rollDistance, 0, 0); ;
                     timeRolling = 0;
+                    rollingLeft = true;
                 }
+
             }
         }
 
+        timeRolling += Time.deltaTime;
+
+            /* Rolling movement */
+        if (rollingRight == true)
+        {
+            if (timeRolling < rollDuration)
+            {
+                transform.Translate(Vector3.right * Time.deltaTime * rollingSpeed);
+            }
+
+            if (timeRolling > rollDelay)
+            {
+                rollingRight = false;
+            }
+        }
+
+        if (rollingLeft == true)
+        {
+            if (timeRolling < rollDuration)
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * rollingSpeed);
+            }
+
+            if (timeRolling > rollDelay)
+            {
+                rollingLeft = false;
+            }
+        }
+
+            /* Walking and Running Functions */
         if (crouchOn == false && inJump == false)
         {
             if (runningOn == true)
@@ -81,41 +224,118 @@ public class MovementScript : MonoBehaviour {
                 {
                     transform.Translate(Vector3.left * Time.deltaTime * runningSpeed);
                 }
+
+                if ((Input.GetKey(KeyCode.D) || (Input.GetKey(KeyCode.A))))
+                {
+                    runTime += Time.deltaTime;
+                }
+                
+                else
+                {
+                    runTime = 0;
+                }
             }
 
             else
             {
                 if (Input.GetKey(KeyCode.D))
                 {
-                    transform.Translate(Vector3.right * Time.deltaTime);
+                    //transform.Translate(Vector3.right * Time.deltaTime);
+                    rb.AddForce(Vector3.right * walkSpeed);
                 }
 
                 if (Input.GetKey(KeyCode.A))
                 {
-                    transform.Translate(Vector3.left * Time.deltaTime);
+                    //transform.Translate(Vector3.left * Time.deltaTime);
+                    rb.AddForce(Vector3.left * walkSpeed);
                 }
             }
         }
 
-        airTime += Time.deltaTime;
-        if (airTime > 3)
+            /* If crouch is on, jumping features are disabled */
+            // Add "|| gunMode == true" for when gunMode is implemented!
+        if (crouchOn == true)
+        {
+
+        }
+
+        else
         {
             if (inJump == true)
             {
-                if (runningOn == true)
-                {
-                    transform.Translate(0, 1, 0);
-                    airTime = 0;
-                    inJump = false;
-                }
+                airTime += Time.deltaTime;
 
+                    /* Stationary Jump */
+                if (runningOn == false)
+                {
+                    if (airTime < jumpDuration)
+                    {
+                        rb.AddForce(new Vector3(0, sJumpHeight) * sJumpSpeed);
+                    }
+                    //  Dictates how long the transform lasts for the jump.
+
+                    else
+                    {
+                        if (airTime > jumpDelay)
+                        {
+                            airTime = 0;
+                            inJump = false;
+                        }
+                    }
+                    //  Acts as cooldown for after the jump.
+                }
+                    /* Running Jump Test */
                 else
                 {
-                    transform.Translate(0, 1.5f, 0);
-                    airTime = 0;
-                    inJump = false;
+                    if (airTime < jumpDuration)
+                    {
+                        if (runTime > rJumpRunUp)
+                        {
+                            rb.AddForce(new Vector3(rJumpDirection, rJumpHeight) * rJumpSpeed);
+                        }
+                        else
+                        {
+                            rb.AddForce(new Vector3(lJumpDirection, lJumpHeight) * lJumpSpeed);
+                        }
+                    }
+
+                    else
+                    {
+                        if (airTime > jumpDelay)
+                        {
+                            airTime = 0;
+                            inJump = false;
+                        }
+                    }
                 }
             }
+        }    
+
+            //if (airTime > 1)
+            //{
+            //    if (inJump == true)
+            //    {
+            //        if (airTime < jumpDelay)
+            //        {
+            //            if (runningOn == true)
+            //            {
+            //                transform.Translate(0, rJumpHeight, 0);
+            //                airTime = 0;
+            //            }
+
+            //            else
+            //            {
+            //                transform.Translate(0, 0.2f, 0);
+            //                rb.AddForce(Vector3.up, ForceMode.Impulse);
+            //                //rb.AddForce(new Vector3(0, sJumpHeight, 0), ForceMode.Force);
+            //                airTime = 0;
+            //            }
+            //        }
+            //        else
+            //        {
+            //            inJump = false;
+            //        }
+            //    }
+            //}
         }
     }
-}
