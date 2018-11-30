361bfd1f6b4e1209b140aef70a442cf0aef2f4fb
WilliamRobertKidd <40745570+WilliamRobertKidd@users.noreply.github.com>
2018-11-30 18:09:18 +1100

    Added Enemies As Prefabs || Implemented Shield Recharge Station

---
+++ Flashback 2150/Assets/Scripts/RechargeStation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RechargeStation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.transform.tag == "Player")
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                //other.GetComponent<PlayerHealth>().shield = 5;
+                other.transform.GetComponent<PlayerHealth>().shield = 5;
+            }
+        }
+    }
+}
