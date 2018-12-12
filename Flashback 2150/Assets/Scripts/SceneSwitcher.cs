b2c645c57fd2fa2a079f02bcdad040228a6796d9
Noobtube141 <corey.shepherd141@gmail.com>
2018-12-13 10:44:18 +1100

    Finished Level 2, put game in finishable state

---
+++ Flashback 2150/Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void Level2()
+    {
+        SceneManager.LoadScene("Level 2", LoadSceneMode.Additive);
+    }
+
+    void Endgame()
+    {
+        SceneManager.LoadScene("End Game Scene", LoadSceneMode.Additive);
+    }
+}
